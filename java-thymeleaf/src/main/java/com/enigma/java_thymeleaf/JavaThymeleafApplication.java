dbac654d5f1cb0da7c70d5105db65bd0d1fa2bff
Ramadhan <dev.ramddd@gmail.com>
2024-11-15 22:32:55 +0700

    [Init] Project-1 Java Thymeleaf

---
+++ java-thymeleaf/src/main/java/com/enigma/java_thymeleaf/JavaThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.java_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaThymeleafApplication.class, args);
+	}
+
+}
